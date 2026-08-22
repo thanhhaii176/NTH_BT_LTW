5f60413f0d4eb94b315688b905e2e5c8dfb831a4
Thanh Hai <hainguyen17062006@gmail.com>
2026-08-22 20:03:56 +0700

    Day02

---
+++ NTH_Day02/MyAppMVC/MyAppMVC/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyAppMVC.Models;
+
+namespace MyAppMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var product = new List<Product>
+            {
+                new Product
+                {
+                    Id = 1,
+                    Name = "Product 1",
+                    Price = 500000,
+                    CreatedAt = new DateTime(2020, 12, 25),
+                    Image = "product1.jpg"
+                },
+                new Product
+                {
+                    Id = 2,
+                    Name = "Product 2",
+                    Price = 700000,
+                    CreatedAt = new DateTime(2020, 12, 25),
+                    Image = "product2.jpg"
+                },
+                new Product
+                {
+                    Id = 3,
+                    Name = "Product 3",
+                    Price = 550000,
+                    CreatedAt = new DateTime(2020, 12, 25),
+                    Image = "product3.jpg"
+                },
+                new Product
+                {
+                    Id = 4,
+                    Name = "Product 4",
+                    Price = 550000,
+                    CreatedAt = new DateTime(2020, 12, 25),
+                    Image = "product4.jpg"
+                }
+            };
+            return View(product);
+        }
+    }
+}
---
+++ NTH_Day02/MyAppMVC/MyAppMVC/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace MyAppMVC.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Image {  get; set; }
+    }
+}
